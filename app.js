b96f670fde52f50e1bd2c7bad69fddfe7927c9c1
xredocx215sevlanevets <stevenalves512@gmail.com>
2020-04-05 14:46:33 -0100

    impl get tour route

--- app.js
+++ app.js
@@ -21,6 +21,24 @@ app.get('/api/v1/tours', (req, res) => {
   });
 });
 
+app.get('/api/v1/tours/:id', (req, res) => {
+  // store the id from params and converte string to number
+  const id = req.params.id * 1;
+  const tour = tours.find((el) => el.id === id);
+
+  if (!tour) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID',
+    });
+  }
+
+  res.status(200).json({
+    status: 'sucess',
+    tour,
+  });
+});
+
 app.post('/api/v1/tours', (req, res) => {
   const newId = tours[tours.length - 1].id + 1;
   const newTour = Object.assign({ id: newId }, req.body);
