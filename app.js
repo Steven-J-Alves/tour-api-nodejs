1e067d2b4989fcb87455b18d75313dde31f8f8eb
xredocx215sevlanevets <stevenalves512@gmail.com>
2020-04-05 16:21:45 -0100

    add morgan middleware

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 var fs = require('fs');
 const express = require('express');
+const morgan = require('morgan');
 
 const app = express();
+
+// MIDDLEWARES
+app.use(morgan('dev'));
+
 app.use(express.json());
 
 app.use((req, res, next) => {
@@ -19,6 +24,7 @@ const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
 
+// ROUTES HANDLERS
 const getAllTours = (req, res) => {
   res.status(200).json({
     // JSEND to format the response json
@@ -105,6 +111,7 @@ const deleteTour = (req, res) => {
 // app.patch('/api/v1/tours/:id', updateTour);
 // app.delete('/api/v1/tours/:id', deleteTour);
 
+// ROUTES
 app.route('/api/v1/tours').get(getAllTours).post(createTour);
 
 app
@@ -113,6 +120,7 @@ app
   .patch(updateTour)
   .delete(deleteTour);
 
+// SERVER
 const port = 3000;
 app.listen(port, () => {
   console.log(`App running on port ${port}...`);
