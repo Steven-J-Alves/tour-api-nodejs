1e968f56551f65163eb0772393cccc3ce72ed79f
xredocx215sevlanevets <stevenalves512@gmail.com>
2020-04-05 14:30:21 -0100

    impl. get and post tour

--- app.js
+++ app.js
@@ -3,6 +3,45 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.json());
+
+// READ DATA TOURS
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({
+    // JSEND to format the response json
+    status: 'sucess',
+    result: tours.length,
+    data: {
+      tours,
+    },
+  });
+});
+
+app.post('/api/v1/tours', (req, res) => {
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+
+  tours.push(newTour);
+
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({
+        status: 'sucess',
+        data: {
+          tour: newTour,
+        },
+      });
+    }
+  );
+});
+
+app;
 const port = 3000;
 app.listen(port, () => {
   console.log(`App running on port ${port}...`);
