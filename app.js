e4e3d1c0c90c13f164d64131f13badd095ffa9ee
xredocx215sevlanevets <stevenalves512@gmail.com>
2020-04-06 12:04:31 -0100

    Environment Variables

--- app.js
+++ app.js
@@ -7,8 +7,13 @@ const userRouter = require('./routes/userRoutes');
 const app = express();
 
 // Middlewares
-app.use(morgan('dev'));
+console.log(process.env.NODE_ENV);
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
 app.use(express.json());
+
 // Serving static files
 app.use(express.static(`${__dirname}/public`));
 
@@ -18,7 +23,7 @@ app.use((req, res, next) => {
 });
 
 app.use((req, res, next) => {
-  req.requestTime = new Date().toISOString();
+  console.log((req.requestTime = new Date().toISOString()));
   next();
 });
 
