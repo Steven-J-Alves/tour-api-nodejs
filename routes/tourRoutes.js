00ffdfc88c4444f48b9e6b53a80d54fb935bd85e
xredocx215sevlanevets <stevenalves512@gmail.com>
2020-04-07 10:33:59 -0100

    delete comments

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -6,11 +6,6 @@ const tourController = require('../controllers/tourController');
 // Params Middleware for route id
 router.param('id', tourController.checkId);
 
-// Create a checkBody middleware
-// Check if body contains the name and price property
-// If not, send back 400(bad request)
-// Add it to the post handler stack
-
 router
   .route('/')
   .get(tourController.getAllTours)
