05bdeef9b0db6d5ea336c86a780cd207148b6af3
xredocx215sevlanevets <stevenalves512@gmail.com>
2020-04-07 18:20:11 -0100

    refatoring console log

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.use((req, res, next) => {
 });
 
 app.use((req, res, next) => {
-  console.log((req.requestTime = new Date().toISOString()));
+  req.requestTime = new Date().toISOString();
   next();
 });
 
