0cdeaec4c865926bb24d24861104b0e38a89a305
xredocx215sevlanevets <stevenalves512@gmail.com>
2020-04-07 10:33:38 -0100

    creating tour model and tour documents

--- server.js
+++ server.js
@@ -18,6 +18,42 @@ mongoose
   })
   .then(() => console.log('DB connection sucessfull'));
 
+// Tour Schema
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A tour must have a name'],
+    unique: true,
+  },
+  rating: {
+    type: Number,
+    default: 4.5,
+  },
+  price: {
+    type: Number,
+    required: [true, 'A tour must have a price'],
+  },
+});
+
+// Create model Tour from tourSchema
+const Tour = mongoose.model('Tour', tourSchema);
+
+// Document from Tour model
+const testTour = new Tour({
+  name: 'The Park Camper',
+  price: 997,
+});
+
+// save it in db; return a promise
+testTour
+  .save()
+  .then((doc) => {
+    console.log(doc);
+  })
+  .catch((err) => {
+    console.log('ERROR: ', err);
+  });
+
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`App running on port ${port}...`);
