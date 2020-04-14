3fb690528c00f8140714503d595a202a22b1ccc4
xredocx215sevlanevets <stevenalves512@gmail.com>
2020-04-14 09:41:19 -0100

    Aggregation pipeline matching and grouping

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -110,3 +110,44 @@ exports.deleteTour = async (req, res) => {
     });
   }
 };
+
+// FIXME: $groups is not allowed in this atlas tier
+// Gettting stats using aggregation pipeline
+exports.getTourStats = async (req, res) => {
+  try {
+    const stats = await Tour.aggregate([
+      {
+        $match: { ratingsAverage: { $gte: 4.5 } },
+      },
+      {
+        $groups: {
+          _id: { $toUpper: '$difficulty' },
+          numTours: { $sum: 1 },
+          numRatings: { $sum: '$ratingsQuantity' },
+          avgRating: { $avg: '$ratingsAverage' },
+          avgPrice: { $avg: '$price' },
+          minPrice: { $min: '$price' },
+          maxPrice: { $max: '$price' },
+        },
+      },
+      {
+        $sort: { avgPrice: 1 },
+      },
+      // {
+      //   $match: { _id: { $ne: 'EASY' } },
+      // },
+    ]);
+
+    res.status(200).json({
+      status: 'sucess',
+      data: {
+        stats,
+      },
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'fail',
+      message: err,
+    });
+  }
+};
